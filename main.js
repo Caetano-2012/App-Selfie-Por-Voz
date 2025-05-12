46cdc6bad223738810e727b74d316ac0535a1a07
Caetano Bordin <hyperplayer2012@gmail.com>
2025-05-12 14:44:48 -0300

    Selfie Por Voz-Save 3

---
+++ main.js
@@ -0,0 +1,10 @@
+let SpeechRecognition = window.webkitSpeechRecognition;
+let recognition = new SpeechRecognition();
+function start() {
+    document.getElementById("textbox").innerHTML="";
+    recognition.start();
+}
+recognition.onresult = function(event) {
+let content = event.results[0][0].transcript;
+document.getElementById("textbox").innerHTML = content;
+}
